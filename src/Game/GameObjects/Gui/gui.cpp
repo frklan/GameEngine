582ef56bff557b0e832d03c270337b7e5a2636be
Fredrik <fredrik.k.andersson@gmail.com>
2019-02-01 20:16:15 +0100

    Removed demo windows

--- src/Game/GameObjects/Gui/gui.cpp
+++ src/Game/GameObjects/Gui/gui.cpp
@@ -25,17 +25,23 @@ void Gui::update(const sf::Time gameTime) {
 
 void Gui::render(sf::RenderTarget& target, sf::Time gameTime) {
   auto& window{getScene().getGameEngine().getWindow()};
+
   ImGui::SFML::Update(window, gameTime);
   
-  if(ImGui::Begin("Sample window", nullptr, 
+  if(ImGui::Begin("Main menu", nullptr, 
     ImGuiWindowFlags_NoTitleBar | 
-    ImGuiWindowFlags_NoBackground | 
+    ImGuiWindowFlags_NoBackground |
     ImGuiWindowFlags_NoResize | 
     ImGuiWindowFlags_NoMove)) {
   
+    auto winSize{window.getSize()};
+    ImVec2 dialogSize {195, 400};
+    ImGui::SetWindowSize(dialogSize);
+    ImGui::SetWindowPos(ImVec2(winSize.x / 2 - dialogSize.x / 2, winSize.y / 2 - dialogSize.y / 2), true);
+
     const Intro& intro = dynamic_cast<const Intro&>(getScene());
     ImGui::PushFont(intro.getImFont());
-    
+
     if (ImGui::Button("Start Game!", {175,55})) {
       this->getScene().getGameEngine().addScene("game", std::make_unique<GameScene>(this->getScene().getGameEngine()));
       this->getScene().getGameEngine().switchScene("game");
@@ -48,16 +54,8 @@ void Gui::render(sf::RenderTarget& target, sf::Time gameTime) {
 
     ImGui::PopFont();
   }
+  ImGui::End();
   
-  ImGui::End();
-
-  ImGui::ShowDemoWindow();
-  ImGui::ShowUserGuide();
-
-  ImGui::Begin("Window2");
-  ImGui::LabelText("Hello World!", "");
-  ImGui::End();
-
   ImGui::SFML::Render(window);
 }
 void Gui::handleEvent(const sf::Event& e) {
